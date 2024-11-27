3e199f8fa82fe012b59a2f0764e5bfee67e4b5e0
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-11-28 00:27:26 +0200

    created the results component, and display all the results in the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 50000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleChangeInput(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-      <Header />;
-      <UserInput />;
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChangeInput} />
+      {!isInputValid && (
+        <p className="center">Please enter a duration greater than 0</p>
+      )}
+      {isInputValid && <Results userInput={userInput} />}
     </>
   );
 }
